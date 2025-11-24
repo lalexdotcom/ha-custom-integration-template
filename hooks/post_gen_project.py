9dc535b3e86b7b13985776244ec21638045ddb8a
LAlex <lalex@lalex.com>
2025-11-24 17:22:30 +0000

    Try copying, will it override

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,11 +5,20 @@ import os
 
 
 def move_template():
-    folder_entries = os.listdir(os.getcwd())
+    working_dir = os.getcwd()
+    # folder_entries = os.listdir(os.getcwd())
 
-    for folder_entry in folder_entries:
-        shutil.copy(folder_entry, "..")
+    # for folder_entry in folder_entries:
+    #     shutil.copy(folder_entry, "..")
 
+    for item in os.listdir(working_dir):
+        source = os.path.join(working_dir, item)
+        target = os.path.join(working_dir, item)
+        if os.path.isdir(source):
+            shutil.copytree(source, target)
+        else:
+            shutil.copy2(source, target)
+    
     os.rmdir(os.getcwd())
 
 
