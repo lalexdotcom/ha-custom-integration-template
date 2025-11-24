a6afc849b469f3bbda89b0d6ac6de38bb883257f
LAlex <lalex@lalex.com>
2025-11-24 16:29:19 +0000

    Change template variable names and add repository_owner Start REAADME.MD template

--- hooks/pre_prompt.py
+++ hooks/pre_prompt.py
@@ -8,15 +8,21 @@ def get_repository_url():
     try:
         cwd = os.getcwd()
         print("cwd is %s", cwd)
-        print("Get repository URL from %s", os.environ['CUSTOM_INTEGRATION_ROOT'])
+        print("Get repository URL from %s",
+              os.environ['CUSTOM_INTEGRATION_ROOT'])
         os.chdir(os.environ['CUSTOM_INTEGRATION_ROOT'])
-        full_url = re.sub(r'(.*)(\.git)$', "\\1",
-                          subprocess.getoutput(["git config --get remote.origin.url"]))
+        repository_url = re.sub(r'(.*)(\.git)$', "\\1",
+                                subprocess.getoutput(["git config --get remote.origin.url"]))
+        repository_owner = re.sub(
+            r'https://github.com/(.*)/.*', "\\1", repository_url)
+        repository_name = re.sub(
+            r'https://github.com/.*/(.*)', "\\1", repository_url)
         os.chdir(cwd)
+
         return {
-            "repository_url": full_url,
-            "github_code_owner": f"@{re.sub(
-                r'https://github.com/(.*)/.*', "\\1", full_url)}"
+            "repository_url": repository_url,
+            "repository_owner": repository_owner,
+            "repository_name": repository_name
         }
     except Exception:
         return False
@@ -30,8 +36,9 @@ if __name__ == "__main__":
     if repository_infos:
         with open("cookiecutter.json", "r+") as file_content:
             data = json.load(file_content)
-            data["github_url"] = repository_infos["repository_url"]
-            data["code_owner"] = repository_infos["github_code_owner"]
+            data["repository_url"] = repository_infos["repository_url"]
+            data["repository_owner"] = repository_infos["repository_owner"]
+            data["repository_name"] = repository_infos["repository_name"]
             file_content.seek(0)
             json.dump(data, file_content, indent=4)
             file_content.truncate()
