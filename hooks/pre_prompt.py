bcb122cc77725371ab544ee5d28ebe84577ec582
LAlex <lalex@lalex.com>
2025-11-24 15:45:39 +0000

    Trace environment for test

--- hooks/pre_prompt.py
+++ hooks/pre_prompt.py
@@ -7,24 +7,31 @@ import subprocess
 def get_repository_url():
     try:
         cwd = os.getcwd()
+        print("cwd is %s", cwd)
         os.chdir("..")
         print("Get repository URL from %s", os.getcwd())
-        full_url = subprocess.getoutput(["git config --get remote.origin.url"])
+        full_url = re.sub(r'(.*)(\.git)$', "\\1",
+                          subprocess.getoutput(["git config --get remote.origin.url"]))
         os.chdir(cwd)
-        return re.sub(r'(.*)(\.git)$', "\\1", full_url)
+        return {
+            "github_url": full_url,
+            "code_owner": f"@{re.sub(
+                r'https://github.com/(.*)/.*', "\\1", full_url)}"
+        }
     except Exception:
         return False
 
 
 if __name__ == "__main__":
-    github_url = get_repository_url()
-    print(github_url)
-    if github_url:
+    print(os.environ)
+    repository_infos = get_repository_url()
+    print(repository_infos)
+    if repository_infos:
         with open("cookiecutter.json", "r+") as file_content:
             data = json.load(file_content)
-            data["github_url"] = github_url
-            data["code_owner"] = f"@{re.sub(
-                r'https://github.com/(.*)/.*', "\\1", github_url)}"
-            file_content.seek(0)
-            json.dump(data, file_content, indent=4)
-            file_content.truncate()
+            # data["github_url"] = repository_infos
+            # data["code_owner"] = f"@{re.sub(
+            #     r'https://github.com/(.*)/.*', "\\1", repository_infos)}"
+            # file_content.seek(0)
+            # json.dump(data, file_content, indent=4)
+            # file_content.truncate()
