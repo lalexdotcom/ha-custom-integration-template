9d3421e6c4361267c50c5b39dd6489d36b304f69
LAlex <lalex@lalex.com>
2025-11-24 12:25:44 +0100

    Try new hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,13 +3,10 @@ import os
 
 
 def move_template():
-    cur_dir = os.getcwd()  # current dir path
-    tmp_dir = os.path.join(cur_dir, "{{cookiecutter.project_name}}")
-    list_dir = os.listdir(tmp_dir)
-    dest = cur_dir
+    list_dir = os.listdir(os.getcwd())
 
     for folder_entry in list_dir:
-        shutil.move(tmp_dir, dest)
+        shutil.move(folder_entry, "..")
 
 
 if __name__ == "__main__":
